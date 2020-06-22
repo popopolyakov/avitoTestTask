8c62e58f99a5f0049d93e8c7a1e60c128fe200af
CUDDLER <bagldya@gmail.com>
2020-06-22 17:18:57 +0300

    Проверка количества контрибьбюторов

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -34,11 +34,12 @@ function RepoCard(props) {
             let resLang = await getInfo(props.repo.languages_url)
             setLanguage(Object.keys(resLang))
             console.log(resLang)
-            let resContribtors = await getInfo(props.repo.contributors_url)
-            console.log(resContribtors)
+            let resContributors = await getInfo(props.repo.contributors_url)
+            console.log(resContributors)
             let topTenContributers = []
-            for (let i = 0; i < 10; i++){
-                topTenContributers.push(resContribtors[i].login)
+            console.log(resContributors)
+            if (!!resContributors.length) {
+                topTenContributers = (new Array(resContributors.length > 10 ? 10 : resContributors.length)).fill('').map((contributor, i) =>  resContributors[i].login )
             }
             setContributors(topTenContributers)
             console.log(JSON.stringify(Object.keys(resLang)))
@@ -85,14 +86,14 @@ function RepoCard(props) {
                         <p>Последний коммит в {lastCommit[0]}   {lastCommit[1]}</p>
                     </div>
                 <div className='RepoCard__ownerInfo'>
-                        <h3>Автор: <a href={ownerInfo.html_url}>{ownerInfo.login}</a></h3>
+                        <h3>Автор: <a href={ownerInfo.html_url} rel="noopener noreferrer" target='_blank'>{ownerInfo.login}</a></h3>
                         <img className='RepoCard__ownerImg' alt="Фото владельца репозитория" src={ownerInfo.avatar_url}></img>
                     </div>
             </div>
             <div className='RepoCard__description'>
                 <p><b>Используемые языки: </b>{load ? (<span>Загрузка</span>) : !!languages.length ? languages.map((item, i) => <span key={item}>{`${item}${i === languages.length-1 ? '' : ','}  `}</span>): 'Никакой из языков программирования не использовался'}</p>
-                <p><b>Краткое описание репозитория: </b>{repoCard.description}</p>
-                <p><b>10 активных контрибьюторов: </b>{load ? (<span>Загрузка</span>): contributors.map((item, i) => <span key={item}>{`${item}${i === 9 ? '' : ','}  `}</span>)}</p>
+                <p><b>Краткое описание репозитория: </b>{!!repoCard.description ? repoCard.description : 'Описания нет'}</p>
+                <p><b>10 активных контрибьюторов: </b>{load ? (<span>Загрузка</span>): !!contributors.length ? contributors.map((item, i) => <span key={item}>{`${item}${i === contributors.length-1 ? ' (это был последний из Contributor\'ов)' : ','}  `}</span>) : 'Нет активных контрибьюторов'}</p>
             </div>
         </div>
     );
